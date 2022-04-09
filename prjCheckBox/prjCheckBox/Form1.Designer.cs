4bed3d40f6d450650fd5c6ef0d2c901b283ba212
Geísa Cardoso <86630228+geisacardoso@users.noreply.github.com>
2022-04-08 22:46:42 -0300

    Add files via upload

---
+++ prjCheckBox/prjCheckBox/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace prjCheckBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.CheckedListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(12, 23);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(409, 202);
+            this.lista.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(126, 243);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(163, 44);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 443);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Exercício CheckedListBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox lista;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ prjCheckBox/prjCheckBox/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjCheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // declara a matriz com os valores
+            string[] linguagens = new string[] { "Cobo", "Assembler", "Java", "C Sharp" };
+            // percorre a lista e preenche o listbox
+            foreach (var linguagem in linguagens)
+            {
+                lista.Items.Add(linguagem);
+            }
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string linguagens = "voce selecionou: \n";
+            for(int i = 0; i < lista.Items.Count; i++)
+            {
+                CheckState estado = lista.GetItemCheckState(i);
+                if (estado == CheckState.Checked) linguagens += lista.Items[i].ToString() + "\n";
+            }
+            MessageBox.Show(linguagens);
+        }
+    }
+}
